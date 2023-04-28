495090daf255cd2571400f3f9f96209c9bdb782a
Hylea6789 <72920934+Hylea6789@users.noreply.github.com>
2023-04-28 16:13:47 +0100

    added clever search

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import re
 
 from NyaaPy.nyaa import Nyaa
 
@@ -15,15 +16,17 @@ class RegularSearch:
             self,
             name: str,
             episode: int,
+            season: int,
             quality: str = "",
             translate_team: str = ""
     ):
         self._name = name.lower()
         self._episode = str(episode)
+        self._season = str(season)
         self._quality = quality.lower()
         self._translate_team = translate_team.lower()
 
-    def search(self) -> str:
+    def search(self) -> object:
         # searches a torrent on nyaa.si from the parameters it was specified
         nyaa = Nyaa()
         results = nyaa.search(keyword=" ".join([self._name, self._episode, self._translate_team, self._quality]))
@@ -32,7 +35,21 @@ class RegularSearch:
         if len(results) == 0:
             return None
         else:
-            return results[0]
+            return self.search_result(results)
+
+    def search_result(self, results: list) -> object:
+        # checks if the ep number and season check a certain pattern with regex
+        len_results = len(results)
+        i = 0
+        regex = "".join(["(- 0{0,1}", self._episode, ")|(S0{0,1}1E0{0,1}", self._season, ")"])
+        while i < len_results:
+            if re.search(regex, results[i].name):
+                return results[i]
+            else:
+                i += 1
+
+        # The good torrent name wasn't found in any
+        return None
 
 
 class ParameterJson:
@@ -59,13 +76,14 @@ class ParameterJson:
 
         name = entry["name"]
         episode = entry["episode"]
+        season = entry["season"]
         translate_team = entry["translate_team"]
         quality = entry["quality"]
 
         # while a new episode might be available
         has_new_episode = True
         while has_new_episode:
-            regular_search = RegularSearch(name, episode, translate_team, quality)
+            regular_search = RegularSearch(name, episode, season, translate_team, quality)
             result = regular_search.search()
 
             # there is no episode available
@@ -86,7 +104,7 @@ class ParameterJson:
         for torrent in self._torrent_list:
             download_url = torrent.download_url
             r = requests.get(download_url, allow_redirects=True)
-            open(torrent.name + ".torrent", 'wb').write(r.content)
+            open("torrent/" + torrent.name + ".torrent", 'wb').write(r.content)
             print('"' + torrent.name + '" downloaded', sep="")
 
 
